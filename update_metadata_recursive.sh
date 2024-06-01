7d06a3771f3758e198be7ed062419d823e4ad95a
Alteox.com <50145315+alteoxCOM@users.noreply.github.com>
2024-06-01 22:22:17 +0200

    Update update_metadata_recursive.sh

--- update_metadata_recursive.sh
+++ update_metadata_recursive.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Find all JSON files in the current directory and subdirectories
-find . -type f -name '*.json' | while read jsonfile; do
-  # Extract the directory of the JSON file
+process_file() {
+  jsonfile="$1"
   directory=$(dirname "$jsonfile")
-  
-  # Extract the title from the JSON file
   title=$(jq -r '.title' "$jsonfile")
-
-  # Construct the full path to the media file
   mediafile="$directory/$title"
 
-  # Check if the media file exists
   if [ -f "$mediafile" ]; then
-    # Extract creationTime timestamp from the JSON file
     creationTime=$(jq -r '.creationTime.timestamp' "$jsonfile")
-    
-    # Convert creationTime from timestamp to a readable format using perl
     creationTimeFormatted=$(perl -e "use POSIX qw(strftime); print strftime('%Y:%m:%d %H:%M:%S', localtime($creationTime));")
-    
-    # Update the metadata from JSON and set the creation date, overwrite original
     exiftool -overwrite_original -json="$jsonfile" \
              "-CreateDate=$creationTimeFormatted" \
              "-DateTimeOriginal=$creationTimeFormatted" \
@@ -28,4 +17,8 @@ find . -type f -name '*.json' | while read jsonfile; do
   else
     echo "File $mediafile does not exist."
   fi
-done
+}
+
+export -f process_file
+
+find . -type f -name '*.json' | parallel -j 8 process_file {}
